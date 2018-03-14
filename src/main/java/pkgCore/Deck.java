4b65707077ccda28db5676286684d4e7d349cef6
jakescreen <36495369+jakescreen@users.noreply.github.com>
2018-03-13 21:08:46 -0400

    lab 3

--- src/main/java/pkgCore/Deck.java
+++ src/main/java/pkgCore/Deck.java
@@ -11,15 +11,15 @@ public class Deck {
 	
 	ArrayList<Card> deck = new ArrayList<Card>();
 	
-	public Deck(int numDecks) {
+	public Deck(int numDeck) {
 		
-		for (int i = 0; i < numDecks; i++) {
+		for (int i = 0; i < numDeck; i++) {
 			
-			for (int j = 0; j < 4; j++) {
+			for (eRank rank : eRank.values()) {
 				
-				for (int k = 0; k < 13; k++) {
+				for (eSuit suit : eSuit.values()) {
 					
-					deck.add(new Card(eRank.values()[k], eSuit.values()[j]));
+					deck.add(new Card(rank, suit));
 
 				}
 				
@@ -28,6 +28,29 @@ public class Deck {
 		}
 		
 		
+	}
+	
+	public int getRemaining(Object eNum) {
+		int remainder = 0;
+		if(eNum instanceof eSuit) {
+			for (Card c : this.deck) {
+				if (c.getSuit() == eNum) {
+					remainder += 1;
+				}
+			}
+		}
+		else {
+			for (Card c : this.deck) {
+				if (c.getRank() == eNum) {
+					remainder += 1;
+				}
+			}
+		}	
+		return remainder;
+	}
+	
+	public Card Draw() {
+		return this.deck.remove(0);
 	}
 
 	
